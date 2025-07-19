6a9ee917c0357c290c5c54a75c0f22b0e0fea2f7
Carles Ripoll Melis <cripolldev@gmail.com>
2025-07-19 20:20:19 +0200

    Added web routes needed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [CustomerController::class, 'customers'])->name('customers.index');
+
+Route::prefix('customers')->group(function () {
+    Route::get('/{customerId}', [CustomerController::class, 'customer'])->name('customers.customer');
+
+    Route::get('/add', [CustomerController::class, 'addCustomer'])->name('customers.add');
+    Route::post('/add', [CustomerController::class, 'addCustomerPost'])->name('customers.add.post');
+
+    Route::get('/update', [CustomerController::class, 'updateCustomer'])->name('customers.update');
+    Route::post('/update', [CustomerController::class, 'updateCustomerPost'])->name('customers.update.post');
+
+    Route::get('/delete/{customerId}', [CustomerController::class, 'deleteCustomer'])->name('customers.delete');
+});
+
+Route::prefix('orders')->group(function () {
+    Route::get('/{orderId}', [orderController::class, 'order'])->name('orders.order');
+
+    Route::get('/add', [orderController::class, 'addOrder'])->name('orders.add');
+    Route::post('/add', [orderController::class, 'addOrderPost'])->name('orders.add.post');
+
+    Route::get('/update', [orderController::class, 'updateOrder'])->name('orders.update');
+    Route::post('/update', [orderController::class, 'updateOrderPost'])->name('orders.update.post');
+
+    Route::get('/delete/{orderId}', [orderController::class, 'deleteOrder'])->name('orders.delete');
 });
